9c592a20445ade07f0958a1282676e871bae07bf
Mostafa010967 <hatmmstfy536@gmail.com>
2025-07-26 22:48:54 +0300

    update All Template And Add Frist Task Js

---
+++ Team/Mostafa_Hatem/JavaScript/Tasks/Tasks_From_01_To_09/Task6.js
@@ -0,0 +1,12 @@
+
+
+// console.log = function () {};
+// document.write = function () {};
+
+throw new Error("Stop Execution");  
+
+console.log("Iam In Console");
+document.write("Iam In Page");
+
+
+
